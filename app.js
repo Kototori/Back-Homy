9397e5b154bac196cfc420e7fd278f2ea8d6ae67
leonardo <leonardofixaa@gmail.com>
2020-03-19 23:32:33 -0300

    republicaS

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const app = express(); 
 
-mongoose.connect('mongodb+srv://republica:leo998709@cluster0-rcrnp.mongodb.net/republica?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://republica:leo998709@cluster0-rcrnp.mongodb.net/republicas?retryWrites=true&w=majority',{
     			  
     useNewUrlParser: true,
     useUnifiedTopology: true
