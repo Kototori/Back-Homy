aa67863136440571a68d26c57aab3642c4f9301b
Robinson <robinsonlc201@gmail.com>
2020-03-22 23:31:39 -0300

    robin ultima versão estavel

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose'); 
 const routes = require('./src/routes');
-const cors = require('cors');
+var cors = require('cors');
 const app = express(); 
 
 mongoose.connect('mongodb+srv://republica:leo998709@cluster0-rcrnp.mongodb.net/republica?retryWrites=true&w=majority',{
-    
+    			  
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
+mongoose.connect('mongodb+srv://republica:leo998709@cluster0-rcrnp.mongodb.net/servicos?retryWrites=true&w=majority',{
+    			  
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json());
 app.use(routes);
 
 
