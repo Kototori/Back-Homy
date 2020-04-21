41ff80c57b730440c4fccda565cb7037bd2dd828
Robinson <robinsonlc201@gmail.com>
2020-04-21 17:50:17 -0300

    atualização de erro

--- src/controllers/UserCaronaController.js
+++ src/controllers/UserCaronaController.js
@@ -4,6 +4,9 @@ const Carona = require('../models/Carona');
 module.exports = {
     async index(request,response){
         const userEmail = request.body.email;
+        if (!userEmail){
+            return response.status(401).json({ error: 'email não encontrado' });
+        }
         const caronas = await Carona.find( userEmail );
         return response.json(caronas);
     }
--- src/controllers/UserServicoController.js
+++ src/controllers/UserServicoController.js
@@ -4,6 +4,9 @@ const Servico = require('../models/Servico');
 module.exports = {
     async index(request,response){
         const userEmail = request.body.email;
+        if (!userEmail){
+            return response.status(401).json({ error: 'email não encontrado' });
+        }
         const servicos = await Servico.find( userEmail );
         return response.json(servicos);
     }
