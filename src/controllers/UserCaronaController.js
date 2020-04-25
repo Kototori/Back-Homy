a073efdac02d89db28fb76f9a4c2d9655f308817
Robinson <robinsonlc201@gmail.com>
2020-04-25 17:33:53 -0300

    atualização de params correto

--- src/controllers/UserCaronaController.js
+++ src/controllers/UserCaronaController.js
@@ -3,7 +3,7 @@ const Carona = require('../models/Carona');
 
 module.exports = {
     async index(request,response){
-        const userEmail = request.params("user");
+        const userEmail = request.params.user;
         if (!userEmail){
             return response.status(401).json({ error: 'email não encontrado' });
         }
