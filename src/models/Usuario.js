1626e6c2b0433c1a7aee87dff005f7ccd5dfe3bb
Robinson <robinsonlc201@gmail.com>
2020-05-20 18:44:38 -0300

    confirmação de email

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -8,20 +8,21 @@ const UsuarioSchema = new mongoose.Schema({
     nome: String,
     fotoPerfil: String,
     celular: String,
-    cpf: String, 
-
-
+    cpf: String,
+    confirmed: { type: Boolean, default: false },
+    numConfirm: { type: Number, default: function () { return Math.floor(Math.random() * 1000000); } },
+    nota: Number,
 });
 //encripta a senha
-UsuarioSchema.pre('save', async function(next){
+UsuarioSchema.pre('save', async function (next) {
     if (this.password) {
-      this.password = await bcrypt.hash(this.password, 8);
+        this.password = await bcrypt.hash(this.password, 8);
     };
     next();
 });
 
 //compara senha com a senha criptada no bd
-UsuarioSchema.methods.comparePassword = function(plaintext, callback) {
+UsuarioSchema.methods.comparePassword = function (plaintext, callback) {
     return callback(null, bcrypt.compareSync(plaintext, this.password));
 };
 
